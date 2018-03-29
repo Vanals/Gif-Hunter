d3ab14cc9bb67c750fb8be2c8c33d7c5e4720415
Vanals <marco91vanali@gmail.com>
2018-03-29 13:05:17 +0100

    Create test for SearchBar component, to chek if it render an input tag.
    Create SearchBar component.  Tests green

---
+++ src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchBar = () => {
+  return (
+    <input type='submit' placeholder='Search GIFs'/>
+  )
+}
+
+export default SearchBar;
--- src/components/__tests__/searchBar.test.js
+++ src/components/__tests__/searchBar.test.js
@@ -2,8 +2,19 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SearchBar from '../SearchBar';
 
-describe('SearchBar Component')
+describe('SearchBar Component', () => {
 
-it('renders without crashing', () => {
-  shallow(<SearchBar />);
-});
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(<SearchBar />);
+  });
+
+  it('renders without crashing', () => {
+    wrapper;
+  });
+
+  it('find input tag', () => {
+    expect(wrapper.find('input').length).toEqual(1)
+  });
+
+})
