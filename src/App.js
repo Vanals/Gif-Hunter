95d778e6a457b1c2d07e93564bb9d6f7de5abadd
Vanals <marco91vanali@gmail.com>
2018-03-29 13:53:03 +0100

    Add SearchBar component in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 class App extends Component {
   constructor() {
@@ -11,6 +12,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    // this.fetchGiphy()
   }
 
   fetchGiphy = () => {
@@ -33,6 +35,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <SearchBar />
         {this.state.gifs}
       </div>
     );
