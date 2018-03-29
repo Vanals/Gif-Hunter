fca02eac84056820a29b00f592aac68a4dc8b920
Vanals <marco91vanali@gmail.com>
2018-03-29 13:04:16 +0100

    Add beforeEach method and incapsulated everything in the describe function

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  shallow(<App />);
-});
+describe('App Component', () => {
+
+  /// Mock fetchGiphy ??
+
+  let wrapper
+  beforeEach(function() {
+    wrapper = shallow(<App />)
+  });
+
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+})
