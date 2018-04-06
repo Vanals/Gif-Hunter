56280c458c3038508c39457daf73e3dffe38a6f7
Vanals <marco91vanali@gmail.com>
2018-04-06 15:18:38 +0100

    Next and Previous button now are not hidden anymore
    They get enabled and disabled in base of which slice of gifs you are exploring.
    
    Created DisablePreviousButtonsChecker and DisableNextButtonsChecker
    This two functions return true of falose in base of which slice are you exploring. They results
    are then used to enable or disable the buttons.
    
    Add a p tag which points the number of the page. Repositioned Buttons and page pointer with CSS inline

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import GifFeed from './components/GifFeed';
+import Radium from 'radium';
 
 class App extends Component {
   constructor() {
@@ -9,6 +10,7 @@ class App extends Component {
     this.state = {
       gifs: [],
       gifSlice: [0, 10],
+      page: 1,
     }
   }
 
@@ -36,7 +38,8 @@ class App extends Component {
 
   nextPage = () => {
     let increasedSlice = this.state.gifSlice.map(n => n + 10)
-    this.setState({gifSlice: increasedSlice})
+    let currentPage = this.state.page + 1
+    this.setState({gifSlice: increasedSlice, page: currentPage})
   }
 
   previousPage = () => {
@@ -44,33 +47,72 @@ class App extends Component {
     this.setState({gifSlice: decreasedSlice})
   }
 
+  disablePreviousButtonsChecker = () => {
+    return (this.state.gifSlice[0] === 0) ? true : false
+  }
+
+  disableNextButtonsChecker = () => {
+    return (this.state.gifSlice[1] > this.state.gifs.length) ? true : false
+  }
+
   render() {
+    let disablePreviousButton
+    disablePreviousButton = this.disablePreviousButtonsChecker()
+
+    let disableNextButton
+    disableNextButton = this.disableNextButtonsChecker()
+
     return (
-      <div>
+      <div style={appDivStyle}>
         <h4>Giphy Search</h4>
         <SearchBar fetchGiphy={this.fetchGiphy}/>
 
         {this.gifSlice(this.state.gifSlice[0], this.state.gifSlice[1])}
+        
+        <div style={ButtonsPageStyle}>
+          <div>
+            <input key="NextButton" style={PreviousButtonStyle} type='submit' value='Previous Page' onClick={this.previousPage} disabled={disablePreviousButton} />
+          </div>
 
+          <div>
+            <p style={PageIndicatorStyle}> Page: {this.state.page} </p>
+          </div>
 
+          <div>
+            <input key="PreviousButton" style={NextButtonStyle} type='submit' value='Next Page' onClick={this.nextPage} disabled={disableNextButton}/>
+          </div>
+        </div>
 
-        { (this.state.gifSlice[0] > 0) &&
-          <input style={PreviousNextButtonStyle} type='submit' value='Previous Page' onClick={this.previousPage} />
-        }
-        { (this.state.gifs.length > this.state.gifSlice[1]) &&
-          <input style={PreviousNextButtonStyle} type='submit' value='Next Page' onClick={this.nextPage} />
-        }
 
       </div>
     );
   }
 }
 
-const PreviousNextButtonStyle = {
+const appDivStyle = {
+  marginLeft: '10px',
+}
+
+const ButtonsPageStyle = {
+  display:'flex',
+}
+
+const PageIndicatorStyle = {
+  marginTop: '0px',
+}
+
+const PreviousButtonStyle = {
+  marginRight: '10px',
   ':hover': {
     color: 'rgb(44, 123, 201)',
   }
 }
 
+const NextButtonStyle = {
+  marginLeft: '10px',
+  ':hover': {
+    color: 'rgb(44, 123, 201)',
+  }
+}
 
-export default App;
+export default Radium(App);
